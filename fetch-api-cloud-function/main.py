5cf1bb205b814650064bf09b387384bcfe5cfc2f
ai_dev_guy <geek.dev.sam@gmail.com>
2024-09-13 20:31:04 +0200

    update main.py

--- fetch-api-cloud-function/main.py
+++ fetch-api-cloud-function/main.py
@@ -1,2 +1,18 @@
-def callapi():
-    return 'data'
+import requests
+from flask import jsonify
+import os 
+ 
+def callapi(request):
+    
+    lat= 59.3293
+    lon = 18.0686
+    apiKey = os.getenv('apiKey')
+    apiAdress = os.getenv('apiAdress')
+    url = f'{apiAdress}data/2.5/weather?lat={lat}&lon={lon}&appid={apiKey}'
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return jsonify(response.json())
+    except:
+        raise Exception("API call failed!")
