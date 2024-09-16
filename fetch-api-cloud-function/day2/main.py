4a2ba11786c949d643585052cfc2e2ca8996ae1d
TwoPlusTwoMakeFour <112890961+TwoPlusTwoMakeFour@users.noreply.github.com>
2024-09-16 13:08:24 +0200

    Reorganized for 2nd callapi

---
+++ fetch-api-cloud-function/day2/main.py
@@ -0,0 +1,33 @@
+import requests
+from google.cloud import storage
+import base64
+from flask import jsonify
+import logging
+import os
+ 
+def callapi_day2(request, context):
+    logging.basicConfig(level=logging.INFO)
+    log = logging.getLogger(__name__)
+    try:
+        #Variables for Python
+        lat= 59.3293
+        lon = 18.0686
+        apiKey = os.getenv('API_KEY')
+        apiAdress = 'https://api.openweathermap.org/'
+        url = f'{apiAdress}data/2.5/weather?lat={lat}&lon={lon}&type=hour&appid={apiKey}'
+        response = requests.get(url)
+        log.info('Python Variables set')
+        #Variables For GCS
+        client = storage.Client()
+        storage_name = 'dataengineering-projektarbete-bucket'
+        bucket = client.bucket(storage_name)
+        item = bucket.blob('day2_weather.json')
+        log.info('GCS Variables set')
+        #Upload
+        item.upload_from_string(response.content)
+        log.info(f'Upload successful! Status code: {response.status_code}')
+        return jsonify(response.json())
+    
+    except Exception as e:
+        log.error(f'Upload failed! Status code: {e}')
+        return None
