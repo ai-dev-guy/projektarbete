32b73f61ef6ff407da3323b9fca61b731089daa7
ai_dev_guy <geek.dev.sam@gmail.com>
2024-09-17 14:54:44 +0200

    change the api from openweathermap to weatherapi

--- fetch-api-cloud-function/day2/main.py
+++ fetch-api-cloud-function/day2/main.py
@@ -4,18 +4,28 @@ import base64
 from flask import jsonify
 import logging
 import os
+import datetime
  
 def callapi_day2(request, context):
     logging.basicConfig(level=logging.INFO)
     log = logging.getLogger(__name__)
     try:
         #Variables for Python
-        lat= 59.3293
-        lon = 18.0686
-        apiKey = os.getenv('API_KEY')
-        apiAdress = 'https://api.openweathermap.org/'
-        url = f'{apiAdress}data/2.5/weather?lat={lat}&lon={lon}&type=hour&appid={apiKey}'
-        response = requests.get(url)
+        # lat= 59.3293
+        # lon = 18.0686
+        # apiKey = os.getenv('API_KEY')
+        # apiAdress = 'https://api.openweathermap.org/'
+        # url = f'{apiAdress}data/2.5/weather?lat={lat}&lon={lon}&type=hour&appid={apiKey}'
+
+        today = datetime.date.today()
+        two_days_ago = today - datetime.timedelta(days=2)
+        twodaysago_date = two_days_ago.strftime("%Y-%m-%d")
+        today_date= today.strftime("%Y-%m-%d")
+
+        weatherapidotcom_key= os.getenv('Weatherapidotcom_Key')
+
+        weatherapi = f'http://api.weatherapi.com/v1/history.json?key={weatherapidotcom_key}&q=Stockholm&dt={twodaysago_date}&end_dt={today_date}'
+        response = requests.get(weatherapi)
         log.info('Python Variables set')
         #Variables For GCS
         client = storage.Client()
