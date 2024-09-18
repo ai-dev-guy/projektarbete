366d7bfcfa164cb7b7fb3ded82f25a72f370d11a
TwoPlusTwoMakeFour <112890961+TwoPlusTwoMakeFour@users.noreply.github.com>
2024-09-18 10:12:14 +0200

    New revision with second api

--- fetch-api-cloud-function/second-api/main.py
+++ fetch-api-cloud-function/second-api/main.py
@@ -5,7 +5,7 @@ from flask import jsonify
 import logging
 import os
  
-def callapi_day1(request, context):
+def fetch_forecast(request, context):
     logging.basicConfig(level=logging.INFO)
     log = logging.getLogger(__name__)
     try:
@@ -14,14 +14,14 @@ def callapi_day1(request, context):
         lon = 18.0686
         apiKey = os.getenv('API_KEY')
         apiAdress = 'https://api.openweathermap.org/'
-        url = f'{apiAdress}data/2.5/weather?lat={lat}&lon={lon}&type=hour&appid={apiKey}'
+        url = f'{apiAdress}data/2.5/forecast?lat={lat}&lon={lon}&type=hour&appid={apiKey}'
         response = requests.get(url)
         log.info('Python Variables set')
         #Variables For GCS
         client = storage.Client()
         storage_name = 'dataengineering-projektarbete-bucket'
         bucket = client.bucket(storage_name)
-        item = bucket.blob('day1_weather.json')
+        item = bucket.blob('forecast.json')
         log.info('GCS Variables set')
         #Upload
         item.upload_from_string(response.content)
@@ -29,5 +29,5 @@ def callapi_day1(request, context):
         return jsonify(response.json())
     
     except Exception as e:
-        log.error(f'Upload failed! Status code: {e}')
+        log.error(f'Upload failed! Exception: {e}')
         return None
