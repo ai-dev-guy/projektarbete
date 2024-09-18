6dfaf496ec346e12da6775c40464cdac8bf01cc5
fraing1 <frans.ingelstam@iths.se>
2024-09-18 16:22:37 +0200

    Update main.py

--- data-cleaning/main.py
+++ data-cleaning/main.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import logging
 
-from datetime import datetime as dt
-
 
 def cleanData(input_filename="raw_weather_data.csv", output_filename="processed_weather_data.csv"):  # Tar default filnamnsargument om inga värden anges
     logging.basicConfig(level=logging.INFO)
@@ -21,28 +19,39 @@ def cleanData(input_filename="raw_weather_data.csv", output_filename="processed_
         log.error(f"An unexpected error occurred while reading the file: {str(e)}")
         raise
 
-    start_date = dt.now() - pd.Timedelta(hours=48)
-    end_date = dt.now()
-
-    required_columns = ['location.localtime_epoch', 'forecast.temp_c', 'forecast.pressure_mb', 'forecast.humidity']
+    required_columns = ['time', 'temp_c', 'pressure_mb', 'humidity']
     missing_columns = [col for col in required_columns if col not in df.columns]
     if missing_columns:
         log.error(f"Error: Missing columns in the input data: {', '.join(missing_columns)}")
         raise ValueError(f"Missing columns: {', '.join(missing_columns)}")
 
-    df = df[(pd.to_datetime(df['location.localtime_epoch'], unit='s') >= start_date) & (pd.to_datetime(df['location.localtime_epoch'], unit='s') <= end_date)]
-
-    df['hour'] = pd.to_datetime(df['location.localtime_epoch'], unit='s').dt.hour
-    df['month'] = pd.to_datetime(df['location.localtime_epoch'], unit='s').dt.month
-    df['temp'] = df['forecast.temp_c']
-    df['temp_lag_1'] = df['forecast.temp_c'].shift(1)
-    df['temp_lag_3'] = df['forecast.temp_c'].shift(3)
-    df['pressure'] = df['forecast.pressure_mb']
-    df['humidity'] = df['forecast.humidity']
-    df['temp_target'] = df['forecast.temp_c'].shift(-1).rolling(24).max()
+    df['hour'] = pd.to_datetime(df['time']).dt.hour
+    df['month'] = pd.to_datetime(df['time']).dt.month
+    df['temp'] = df['temp_c']
+    df['temp_lag_1'] = df['temp_c'].shift(1)
+    df['temp_lag_3'] = df['temp_c'].shift(3)
+    df['pressure'] = df['pressure_mb']
+    df['humidity'] = df['humidity']
+    df['temp_target'] = df['temp_c'].shift(-24)
     
     columns_to_keep = ['hour', 'month', 'temp', 'humidity', 'pressure', 'temp_lag_1', 'temp_lag_3', 'temp_target']
     df = df[columns_to_keep]
 
+    df = df[:-24]
     parsed_df = df.dropna()
     log.info(f"Processed {len(parsed_df)} rows of data")
+
+    try:
+        parsed_df.to_csv(output_filename, index=False)
+        log.info(f"Saved processed data to {output_filename}")
+    except Exception as e:
+        log.error(f"Error saving processed data: {str(e)}")
+        raise
+
+
+if __name__ == "__main__":
+    try:
+        cleanData("raw_weather_data.csv", "processed_weather_data.csv")
+    except Exception as e:
+        logging.error(f"An error occurred: {str(e)}")
+        raise
